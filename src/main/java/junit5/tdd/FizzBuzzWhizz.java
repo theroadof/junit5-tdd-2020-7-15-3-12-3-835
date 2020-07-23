5176f8c532e607292e6c8a0a37906d9c52444e11
TEDDY LIN (ITA-ISDC-ISD-OOCLL/ZHA) <LINTE@OOCL.COM>
2020-07-23 12:40:26 +0800

    [FizzBuzzWhizz][Teddy] feature:implement should_be_fizzbuzz_when_play_say_given_3x5

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -3,16 +3,16 @@ package junit5.tdd;
 import com.google.common.base.Strings;
 
 public class FizzBuzzWhizz {
-    public String say(int number){
+    public String say(int number) {
         String result = "";
-        if(number % 3 == 0 ){
-            result+="Fizz";
+        if (number % 3 == 0) {
+            result += "Fizz";
         }
-        if(number % 5 == 0){
-            result+="Buzz";
+        if (number % 5 == 0) {
+            result += "Buzz";
         }
-        if(number % 7 == 0){
-            result+="Whizz";
+        if (number % 7 == 0) {
+            result += "Whizz";
         }
         return result;
     }
