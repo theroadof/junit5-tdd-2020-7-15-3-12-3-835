9bb6e094858411eb17b9cfa5652db367d8e8f620
TEDDY LIN (ITA-ISDC-ISD-OOCLL/ZHA) <LINTE@OOCL.COM>
2020-07-23 12:35:12 +0800

    [FizzBuzzWhizz][Teddy] feature : implement should_be_whizz_when_play_say_given_7

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -11,6 +11,9 @@ public class FizzBuzzWhizz {
         if(number % 5 == 0){
             result+="Buzz";
         }
+        if(number % 7 == 0){
+            result+="Whizz";
+        }
         return result;
     }
 }
